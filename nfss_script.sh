2502eb5c9e868345872772570dda3f521ef3bd1a
Andrey Tyumin <posthus@yandex.ru>
2020-08-19 17:01:01 +0300

    otus 4th homework 1st try

---
+++ nfss_script.sh
@@ -0,0 +1,14 @@
+yum install nfs-utils -y nfs-utils-lib
+mkdir -p /var/super_shara/upload
+chown nfsnobody /var/super_shara/upload
+sudo systemctl enable rpcbind
+sudo systemctl enable nfs-server
+sudo  systemctl enable nfs-lock
+sudo systemctl enable nfs-idmap
+sudo systemctl start rpcbind
+sudo systemctl start nfs-server
+sudo systemctl start nfs-lock
+sudo systemctl start nfs-idmap
+echo "/var/super_shara 192.168.50.11(rw,sync,root_squash,all_squash)" >> /etc/exports
+exportfs -a
+sudo systemctl restart nfs-server
